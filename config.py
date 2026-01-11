e0892320dfa660239e850f6d680581d66b7fcdcb
katebas <angel_kiss_92@mail.ru>
2026-01-11 17:00:27 +0300

    Первый коммит: добавление базовых файлов

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN="8528613348:AAHAqSpX_QVYzDdsWHGgUgp6Jg2VW_7byWQ"
+API_KEY = "9b333f401e62b87b61983441"
+
+keys= {
+    "рубль":"RUB",
+    "доллар":"USD",
+    "юань":"CNY",
+    "евро":"EUR",
+}
